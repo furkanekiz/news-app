e94c7cd7fec7562277e8bb0b8b6b8c1abbbbb020
furkanekiz <furkanekiz.06@gmail.com>
2023-09-23 19:26:00 +0300

    Data classes were created for the model. Created suspend functions for use cases in the NewsRepository.kt interface. Created execute functions in use case classes. Resource.kt sealed class has been created.

--- app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -1,4 +1,15 @@
 package com.example.newsapp.domain.repository
 
+import com.example.newsapp.data.model.APIResponse
+import com.example.newsapp.data.model.Article
+import com.example.newsapp.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface NewsRepository {
+
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews(): Flow<List<Article>>
 }
--- app/src/main/java/com/example/newsapp/domain/usecase/GetSearchedNewsUseCase.kt
+++ app/src/main/java/com/example/newsapp/domain/usecase/GetSearchedNewsUseCase.kt
@@ -1,6 +1,12 @@
 package com.example.newsapp.domain.usecase
 
+import com.example.newsapp.data.model.APIResponse
+import com.example.newsapp.data.util.Resource
 import com.example.newsapp.domain.repository.NewsRepository
 
 class GetSearchedNewsUseCase(private val newsRepository: NewsRepository) {
+
+    suspend fun execute(searchQuery: String): Resource<APIResponse> {
+        return newsRepository.getSearchedNews(searchQuery)
+    }
 }
