8864ec4b54ec8a1a7ad76656d93a91bc1e333f38
furkanekiz <furkanekiz.06@gmail.com>
2023-09-27 15:23:53 +0300

    Data source and repository functions related to the GetNewsHeadlines use case have been implemented.

---
+++ app/src/main/java/com/example/newsapp/data/repository/dataSource/NewsRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.data.repository.dataSource
+
+import com.example.newsapp.data.model.APIResponse
+import retrofit2.Response
+
+interface NewsRemoteDataSource {
+
+    suspend fun getTopHeadlines(): Response<APIResponse>
+}
